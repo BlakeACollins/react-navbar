e2286ac702feeacf6a05e43c5e5e885fa64c872e
Blake Collins <blakeac369@gmail.com>
2021-03-22 20:16:41 -0700

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
-export const About = () => {
+
+const About = () => {
     return (
-        <div>
-            <h4>About</h4>
+        <div id='about' className='container py-5'>
+            <div className='row'>
+                <div className='col-lg-6 col-xm-12'>
+                    <div className='photo__container'>
+                       
+                    </div>
+                </div>
+
+                <div className='col-lg-6 col-xm-12'>
+                    <h2 className='about__heading pb-3'>About Me</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ut posuere erat. Donec felis ligula, fermentum eu ex ut, maximus dignissim lacus. Donec eleifend mauris nec ultricies fringilla. Proin rhoncus ipsum ut justo vulputate ultricies. Phasellus est eros, tempus a velit eget, tincidunt tempor arcu. Etiam molestie nisi mi, ut vestibulum erat facilisis at. Cras feugiat lectus vel turpis faucibus vestibulum. Integer imperdiet eros et quam commodo, in porttitor nibh interdum. Etiam id tortor metus. Nunc aliquam ultricies odio, dictum lobortis lacus viverra quis. Etiam in sagittis sapien. Curabitur at hendrerit augue. Nullam dictum facilisis erat, id malesuada quam vehicula ut. Etiam purus justo, condimentum sit amet enim sit amet, mollis imperdiet odio.</p>
+                </div>
+            </div>
+            
         </div>
     )
 }
+
+export default About
